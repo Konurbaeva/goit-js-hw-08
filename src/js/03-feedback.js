9a718561f1bf8474a8ca280b08235061280d9099
Konurbaeva <konurbaevamadina@gmail.com>
2022-02-04 18:52:34 +0100

    add form locators

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,16 +1,37 @@
+import throttle from 'lodash.throttle';
 
-//const feedbackForm = document.querySelector('.feedback-form');
-
-const input = document.querySelector('input');
-const email = document.querySelector('[name="email"]');
-const message = document.querySelector('[name="message"]');
+ const STORAGE_KEY = 'feedback-form-state';
 
 
-input.addEventListener('input', localStorageSetter);
+const refs = {
+    form: document.querySelector('.feedback-form'),
+   // textarea: document.querySelector('.feedback-form textarea'),
+    email: document.querySelector('[name="email"]'),
+    message: document.querySelector('[name="message"]')
+  };
+  
+  refs.form.addEventListener('submit', onFormSubmit);
+  //refs.textarea.addEventListener('input', throttle(onTextareaInput, 200));
+  refs.form.addEventListener('input', onTextareaInput);
+  refs.email.addEventListener('input', throttle(onEmailInput, 200));
+  refs.message.addEventListener('input', throttle(onMessageInput, 200));
 
-function localStorageSetter(event){
-    console.log('event', event.target.value);
+  function onFormSubmit(e){
+      e.preventDefault();
+    
+      console.log('send out the form')
+  }
 
-    localStorage.setItem('email', email.value);
-    localStorage.setItem('message', message.value);
-}
+  function onEmailInput(e) {
+    const emailStorage = e.target.value;
+  
+    localStorage.setItem(STORAGE_KEY, emailStorage);
+  }
+
+  function onMessageInput(){
+    const messageStorage = e.target.value;
+  
+    localStorage.setItem(STORAGE_KEY, messageStorage);
+  }
+ 
+
