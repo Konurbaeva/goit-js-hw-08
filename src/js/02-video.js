13c46810d962d997c879a495d625633948beaa34
Konurbaeva <konurbaevamadina@gmail.com>
2022-02-04 14:30:50 +0100

    init commit for feedback form

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,39 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash/throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+
+const player = new Player(iframe);
+
+player.on('play', function() {
+    console.log('played the video!');
+});
+
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
+
+const onPlay = function(data) {
+    console.log("data: ", data)
+};
+
+const onTimeUpdate = throttle(function(data) {
+    window.localStorage.setItem('videoplayer-current-time', JSON.stringify(data.seconds))
+}, 1000)
+
+player.setCurrentTime(window.localStorage.getItem('videoplayer-current-time')).then(function(seconds) {
+   console.log("seconds", seconds);
+
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+
+        default:          
+            break;
+    }
+});
+
+player.on('play', onPlay);
+player.on('timeupdate', onTimeUpdate);
+player.off('play', onPlay);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,16 @@
 
+//const feedbackForm = document.querySelector('.feedback-form');
+
+const input = document.querySelector('input');
+const email = document.querySelector('[name="email"]');
+const message = document.querySelector('[name="message"]');
+
+
+input.addEventListener('input', localStorageSetter);
+
+function localStorageSetter(event){
+    console.log('event', event.target.value);
+
+    localStorage.setItem('email', email.value);
+    localStorage.setItem('message', message.value);
+}
